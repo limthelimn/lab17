68a82a5eeecf1c360ed6c6004ee5eccba7fe6ead
limthelimn <nuttawan.lim@gmail.com>
2021-02-19 18:01:43 +0700

    lab17/1

--- lab17_1.cpp
+++ lab17_1.cpp
@@ -20,22 +20,59 @@ string toUpperStr(string x){
     return y;
 }
 
-void importDataFromFile(){
-
+void importDataFromFile(string filename,vector<string> &names,vector<int> &scores,vector<char> &grades){
+    ifstream data;
+    data.open(filename.c_str());
+    string file;
+    char check[] = "%[^:]: %d %d %d";
+    char namae[100];
+    int a1,a2,a3;
+    while(getline(data,file)){
+        sscanf(file.c_str(),check,namae,&a1,&a2,&a3);
+        names.push_back(namae);
+        scores.push_back(a1+a2+a3);
+        grades.push_back(score2grade(a1+a2+a3));
+    }
+    data.close();
 }
 
-void getCommand(){
-
+void getCommand(string &command,string &key){
+    string N;
+    int sup;
+    cout << "Please input your command: ";
+    getline(cin,N);
+    sup = N.find_first_of(" ");
+    command = N.substr(0,sup);
+    key = N.substr(sup+1);
 }
 
-void searchName(){
-
+void searchName(vector<string> name,vector<int> scores,vector<char> grades,string key){
+    bool checker = false;
+    for(unsigned int i = 0; i < name.size(); i++){
+        if(key == toUpperStr(name[i])){
+            checker = true;
+            cout << "---------------------------------"<<endl;
+            cout << name[i] << "'s score = " << scores[i] << endl;
+            cout << name[i] << "'s grade = " << grades[i] << endl;
+            cout << "---------------------------------" << endl;
+        }
+    }
+    if(checker == false){
+        cout << "---------------------------------" << endl;
+        cout << "Cannot found." << endl;
+        cout << "---------------------------------" << endl;
+    }
 }
 
-void searchGrade(){
-
+void searchGrade(vector<string> name,vector<int> scores,vector<char> grades,string key){
+    cout << "---------------------------------" << endl;
+    for(unsigned int i = 0; i < grades.size(); i++){
+        if(key[0] == grades[i]){
+            cout << name[i] << " " << "("<<scores[i] << ")" << endl;
+        }
+    }
+    cout << "---------------------------------" << endl;
 }
-
 
 int main(){
     string filename = "name_score.txt";
